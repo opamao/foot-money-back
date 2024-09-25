cd5a8eb0fdb7a70b48476790de054b17bfbfd4e3
Kadhel AMAN <kadhelaman@192.168.1.3>
2024-09-25 01:57:25 +0000

    design tableau de bord

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -18,76 +18,48 @@
             <div class="row clearfix">
                 <div class="col-lg-12">
                     <div class="mb-4">
-                        <h4>Welcome Peter Richards!</h4>
-                        <small>Measure How Fast You’re Growing Monthly Recurring Revenue. <a href="#">Learn
-                                More</a></small>
+                        <h4>Bienvenu Richards!</h4>
                     </div>
                 </div>
             </div>
             <div class="row clearfix row-deck">
-                <div class="col-xl-2 col-lg-4 col-md-6">
+                <div class="col-xl-3 col-lg-4 col-md-6">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Active Cases</h3>
+                            <h3 class="card-title">Nombre de joueurs</h3>
                         </div>
                         <div class="card-body">
                             <h5 class="number mb-0 font-32 counter">31</h5>
-                            <span class="font-12">Measure How Fast... <a href="#">More</a></span>
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-2 col-lg-4 col-md-6">
+                <div class="col-xl-3 col-lg-4 col-md-6">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Pending Tasks</h3>
+                            <h3 class="card-title">Nombre de vote</h3>
                         </div>
                         <div class="card-body">
                             <h5 class="number mb-0 font-32 counter">245</h5>
-                            <span class="font-12">Measure How Fast... <a href="#">More</a></span>
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-2 col-lg-4 col-md-6">
+                <div class="col-xl-3 col-lg-4 col-md-6">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Upcoming Events</h3>
+                            <h3 class="card-title">Dons</h3>
                         </div>
                         <div class="card-body">
-                            <h5 class="number mb-0 font-32 counter">17</h5>
-                            <span class="font-12">Measure How Fast... <a href="#">More</a></span>
+                            <h5 class="number mb-0 font-32 counter">500 000 000 FCFA</h5>
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-2 col-lg-4 col-md-6">
+                <div class="col-xl-3 col-lg-4 col-md-6">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">New Message</h3>
+                            <h3 class="card-title">Nombre utilisateur</h3>
                         </div>
                         <div class="card-body">
                             <h5 class="number mb-0 font-32 counter">12</h5>
-                            <span class="font-12">Measure How Fast... <a href="#">More</a></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-2 col-lg-4 col-md-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Open Requests</h3>
-                        </div>
-                        <div class="card-body">
-                            <h5 class="number mb-0 font-32 counter">19</h5>
-                            <span class="font-12">Measure How Fast... <a href="#">More</a></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-2 col-lg-4 col-md-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Hours Spent</h3>
-                        </div>
-                        <div class="card-body">
-                            <h5 class="number mb-0 font-32 counter">284</h5>
-                            <span class="font-12">Measure How Fast... <a href="#">More</a></span>
                         </div>
                     </div>
                 </div>
@@ -100,13 +72,9 @@
                 <div class="col-xl-4 col-lg-12 col-md-12">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Transaction History</h3>
+                            <h3 class="card-title">Transaction historique</h3>
                             <div class="card-options">
-                                <a href="#" class="card-options-remove" data-toggle="card-remove"><i
-                                        class="fe fe-x"></i></a>
                                 <div class="item-action dropdown ml-2">
-                                    <a href="javascript:void(0)" data-toggle="dropdown" aria-expanded="false"><i
-                                            class="fe fe-more-vertical"></i></a>
                                     <div class="dropdown-menu dropdown-menu-right" x-placement="bottom-end"
                                         style="position: absolute; transform: translate3d(-174px, 25px, 0px); top: 0px; left: 0px; will-change: transform;">
                                         <a href="javascript:void(0)" class="dropdown-item"><i
